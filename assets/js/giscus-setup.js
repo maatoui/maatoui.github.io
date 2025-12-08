195261f80713a029bad90a0517e63cb3502cba5a
maatoui <amine.atoui@gmail.com>
2025-12-08 15:12:37 +0000

    Deploying to gh-pages from @ maatoui/maatoui.github.io@b0d15788d690344623ad47805786fb83985fbf8f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "maatoui/maatoui",
+    "data-repo": "maatoui/maatoui.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
